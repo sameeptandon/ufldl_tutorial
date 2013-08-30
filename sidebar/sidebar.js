c5936ec13fe0e7d29d8991604bf21462291a5fb3
Sameep Tandon <sameep@stanford.edu>
2013-08-30 15:56:02 -0700

    no ~ files

--- sidebar/sidebar.js
+++ sidebar/sidebar.js
@@ -2,7 +2,7 @@
 // this structure specifies the ordering of pages on the sidebar and the pages they link to. 
 var pages = [
 { name: 'Supervised Learning and Optimization', link: '#', type: 'Heading' },
-{ name: 'Linear Regression', link: '#', type: 'Page' },
+{ name: 'Linear Regression', link: 'supervised/LinearRegression', type: 'Page' },
 { name: 'Logistic Regression', link: '#', type: 'Page' },
 { name: 'Vectorization', link: '#', type: 'Page' },
 { name: 'Debugging: Gradient Checking', link: '#', type: 'Page' },
