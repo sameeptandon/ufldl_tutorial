0892b1b3ca9a92135648ace9bfdd1428e511d8d4
Sameep Tandon <sameep@stanford.edu>
2013-08-30 15:14:07 -0700

    updating sidebar

---
+++ sidebar/sidebar.js
@@ -0,0 +1,10 @@
+
+// this structure specifies the ordering of pages on the sidebar and the pages they link to. 
+var pages = [
+{ name: 'Supervised Learning and Optimization', link: '#', type: 'Heading' },
+{ name: 'Linear Regression', link: '#', type: 'Page' },
+{ name: 'Logistic Regression', link: '#', type: 'Page' },
+{ name: 'Vectorization', link: '#', type: 'Page' },
+{ name: 'Debugging: Gradient Checking', link: '#', type: 'Page' },
+{ name: 'Softmax Regression', link: '#', type: 'Page' }
+];
